ed6da69215aed56e94288a983b2036babdf0ab3f
smcrae <sherrie_mcrae@yahoo.com>
2014-01-24 17:02:26 -0800

    Create customer.cs

---
+++ customer.cs
@@ -0,0 +1,18 @@
+ITC172
+======
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public class Customer
+{
+    public string LastName { get; set; }   ///generic properties get and set
+    public string FirstName { get ; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    }
